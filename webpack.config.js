4f672207f1fc2940a2e65e0f3a07e326d9c1f657
Anton Wilhelm <timaschew@gmail.com>
2016-06-15 19:23:37 +0200

    fix production environment

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     publicPath: assetPath
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
